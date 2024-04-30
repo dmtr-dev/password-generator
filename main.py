63416e24497b8aea8d73994acf8e5c242e7b5932
dmtr <93618083+dmtr-dev@users.noreply.github.com>
2024-04-30 21:01:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 import pyperclip
+from ctypes import windll
+
+windll.kernel32.SetConsoleTitleW('Password Generator | by https://t.me/dmtrcrypto')
+print("TG Channel - https://t.me/dmtrcrypto\n\n\n")
 
 chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = 1
@@ -10,3 +14,5 @@ for n in range(number):
         password += random.choice(chars)
     print(password)
     pyperclip.copy(password)
+
+input("\nPress Enter to exit...")
